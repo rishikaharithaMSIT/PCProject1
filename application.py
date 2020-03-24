49b9d45852382f51837dae824737c1a27ff98525
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2020-03-24 19:25:59 +0530

    24th,March:Task-2

--- application.py
+++ application.py
@@ -25,3 +25,8 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        pass
+    return render_template("register.html")
