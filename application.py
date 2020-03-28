b1c6f6b835376d986d0fb4aa18bc96c92521c72d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2020-03-28 18:05:08 +0530

    imported data into books table

--- application.py
+++ application.py
@@ -2,9 +2,7 @@
 Flask App
 """
 import os
-
 import logging
-
 from flask import Flask, session, request, render_template, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine, exc, desc
@@ -33,39 +31,50 @@ db.init_app(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 
-# default router
+"""
+default router
+"""
 @app.route("/")
 def index():
     try:
-        currentUser = session['user']
+        current_user = session['user']
     except:
         return render_template("register.html", data="You must log in continue.")
-    return render_template("homePage.html", data=currentUser)
+    return render_template("homePage.html", data=current_user)
 
-# login
+"""
+login
+"""
 @app.route("/auth", methods=['POST'])
 def auth():
     if request.method == "POST":
         email = request.form['email']
         password = request.form['pswd']
         if email == "" or password == "":
-            return render_template("register.html", data="Please enter username and password to register")
+            return render_template("register.html", data="Please enter\
+             username and password to register")
         queryResultSet = Users.query.filter_by(email=email).first()
         if queryResultSet is not None:
             if bcrypt.verify(password, queryResultSet.password):
                 session['user'] = email
                 return redirect(url_for('index'))
             else:
-                return render_template("register.html", data="Email and password does not match. Try again!")
+                return render_template("register.html", data="Email and\
+                 password does not match. Try again!")
 
-        return render_template("register.html", data="Email does not exists. Try to Register")
-
+        return render_template("register.html", data="Email does not exists.\
+         Try to Register")
+"""
+Logout
+"""
 @app.route("/logout")
 def logout():    
     session.clear()
     return render_template("register.html", data="You logged out sucessfully")
 
-#register page
+"""
+register page
+"""
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -73,21 +82,27 @@ def register():
         email = request.form['email'] 
         password = bcrypt.encrypt(request.form['pswd'])       
         if email == "" or bcrypt.verify("", password):
-            return render_template("register.html", data="Please enter username and password to register")
+            return render_template("register.html", data="Please enter username\
+                and password to register")
         if '@' not in email or '.' not in email:
             return render_template("register.html", data="Please enter a valid email to register")
         try:
             user = Users(email=email, password=password, registerTime=datetime.now())
             db.session.add(user)
             db.session.commit()
-            return render_template("register.html", data="You registered sucessfully! Login to continue.")
+            return render_template("register.html", data="You registered\
+             sucessfully! Login to continue.")
         except exc.IntegrityError:
-            return render_template("register.html", data="The Email you used already exists! Try logging in.")
+            return render_template("register.html", data="The Email you\
+             used already exists! Try logging in.")
         except:
-            logging.debug('exception message', 'something else went wrong in adding a user')
+            logging.debug('exception message', 'something else went wrong\
+             in adding a user')
     return render_template("register.html")
 
-#admin page
+"""
+admin page
+"""
 @app.route("/admin")
 def listUsers():
     queryResultSet = Users.query.order_by(desc(Users.registerTime)).all()
--- models.py
+++ models.py
@@ -8,9 +8,21 @@ class Users(db.Model):
     password = db.Column(db.String, nullable=False)
     registerTime = db.Column(db.DateTime(timezone=True), nullable=False)
 
-def __init__(self, email, password, created) :
+    def __init__(self, email, password, created):
         self.email = email
         self.password = password
         self.registerTime = registerTime
 
+class Books(db.Model):
+    __tablename__ = "books"
+    isbn = db.Column(db.String, primary_key=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
 
+    def __init__(self, isbn, title, author, year) :
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.year = year
+        
