e9d3ca407f6ff4623a05cb08af73070357e158f8
vipulchakravarthy <saivipul15@gmai.com>
2020-04-01 16:47:08 +0530

    Done with test cases for book_details

--- book_details.py
+++ book_details.py
@@ -6,4 +6,6 @@ from models import db, Books
 
 def get_book_details(isbn):
     book = Books.query.get(isbn)
+    if book is None:
+        return None
     return book
--- test_book_details.py
+++ test_book_details.py
@@ -1,10 +1,14 @@
+'''
+This file is for running test cases on
+book_details feature
+'''
 import os
 import unittest
- 
+from flask import Flask
 from models import db
-from application import app
 import book_details
- 
+from flask_sqlalchemy import SQLAlchemy
+
  
 class BasicTests(unittest.TestCase):
  
@@ -14,21 +18,26 @@ class BasicTests(unittest.TestCase):
  
     # executed prior to each test
     def setUp(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-        self.app = app.test_client()
+        app = Flask(__name__)
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        db = SQLAlchemy()
+        db.init_app(app)
+        app.app_context().push() 
   
 ###############
 #### tests ####
 ###############
  
     def test_book_page(self):
-        response = self.app.get('/books/0380795272', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-        # res = book_details.get_book_details('0380795272')
-        # self.assertEqual(res.title,'Krondor: The Betrayal')
+        # response = self.app.get('/books/0380795272', follow_redirects=True)
+        # self.assertEqual(response.status_code, 200)
+        res = book_details.get_book_details('0380795272')
+        self.assertEqual(res.title,'Krondor: The Betrayal')
+        self.assertEquals(res.author, 'Raymond E. Feist')
+
+        res = book_details.get_book_details('1244')
+        self.assertEqual(res, None)
      
 if __name__ == "__main__":
     unittest.main()
